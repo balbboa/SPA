dd5491893dd74b5a7812a8b0a3e2360bcc88fc2c
balbboa <abmm_arthur@hotmail.com>
2021-02-23 22:58:49 -0300

    step 1

---
+++ spa/src/App.tsx
@@ -0,0 +1,12 @@
+import { Bar } from './components/Bar';
+import './styles/global.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <Bar />
+    </div>
+  );
+}
+
+export default App;
---
+++ spa/src/components/Bar.tsx
@@ -0,0 +1,11 @@
+export function Bar() {
+    return (
+        <header className="bar">
+            <span>0%</span>
+            <div>
+                <div style={{ width: '50%' }}></div>
+            </div>
+            <span>100%</span>
+        </header>
+    );
+}
